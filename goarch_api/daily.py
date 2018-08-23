1b92b9bc242a79323311b8f7c4e2ef9bd0303e08
Elliott Pardee <me@vypr.xyz>
2018-08-23 11:45:47 -0400

    Move data-grabbing functions to the classes.

--- goarch_api/daily.py
+++ goarch_api/daily.py
@@ -5,66 +5,59 @@ from goarch_api.models import Reading, Translation
 from goarch_api.saint import Saint
 
 
-def get_data(self):
-    client = HTTPClient()
-
-    resp = client.get(self.path)
-
-    if resp is not None:
-        tree = etree.fromstring(resp.encode())
-        date = tree.find("date").text
-
-        self.formatted_date = tree.find("formatteddate").text
-
-        self.icon = tree.find("icon").text
-        self.fasting = tree.find("fasting").text
-        self.tone = tree.find("tone").text
-
-        self.readings = []
-
-        for reading in tree.find("readings").iterchildren():
-            _id = reading.get("id")
-            _type = reading.find("type").text
-            type_bb = reading.find("typebb").text
-            event = reading.find("event").text
-
-            translation = reading.find("translation")
-            t_lang = translation.get("xml:lang")
-            t_title = translation.find("title").text
-            t_short_title = translation.find("shorttitle").text
-
-            # more often than not, these things have a newline after the text in the element
-            t_clip = translation.find("clip").text.strip()
-
-            converted_translation = Translation(title=t_title, short_title=t_short_title, clip=t_clip, lang=t_lang)
-
-            converted_reading = Reading(_id, _type, type_bb, event, translation=converted_translation, date=date)
-
-            self.readings.append(converted_reading)
-
-        self.lectionary_title = tree.find("lectionarytitle").text.strip()
-
-        self.saints = []
-
-        for saint in tree.find("saintsfeasts").iterchildren():
-            _id = saint.find("id").text
-            converted_saint = Saint(_id)
-
-            self.saints.append(converted_saint)
-
-
 class Daily:
     def __init__(self, date=None):
         self.date = date
-        self.path = "/daily.asp"
+        self._path = "/daily.asp"
         self.public_url = "https://www.goarch.org/chapel"
 
         if date is not None:
-            self.path += f"?date={date}"
+            self._path += f"?date={date}"
             self.public_url += f"?date={date}"
 
-        get_data(self)
+    def get_data(self):
+        client = HTTPClient()
 
+        resp = client.get(self._path)
 
-if __name__ == "__main__":
-    daily = Daily(date="8/15/2018")
+        if resp is not None:
+            tree = etree.fromstring(resp.encode())
+            date = tree.find("date").text
+
+            self.formatted_date = tree.find("formatteddate").text
+
+            self.icon = tree.find("icon").text
+            self.fasting = tree.find("fasting").text
+            self.tone = tree.find("tone").text
+
+            self.readings = []
+
+            for reading in tree.find("readings").iterchildren():
+                _id = reading.get("id")
+                _type = reading.find("type").text
+                type_bb = reading.find("typebb").text
+                event = reading.find("event").text
+
+                translation = reading.find("translation")
+                t_lang = translation.get("xml:lang")
+                t_title = translation.find("title").text
+                t_short_title = translation.find("shorttitle").text
+
+                # more often than not, these things have a newline after the text in the element
+                t_clip = translation.find("clip").text.strip()
+
+                converted_translation = Translation(title=t_title, short_title=t_short_title, clip=t_clip, lang=t_lang)
+
+                converted_reading = Reading(_id, _type, type_bb, event, translation=converted_translation, date=date)
+
+                self.readings.append(converted_reading)
+
+            self.lectionary_title = tree.find("lectionarytitle").text.strip()
+
+            self.saints = []
+
+            for saint in tree.find("saintsfeasts").iterchildren():
+                _id = saint.find("id").text
+                converted_saint = Saint(_id)
+
+                self.saints.append(converted_saint)
--- goarch_api/lectionary.py
+++ goarch_api/lectionary.py
@@ -11,50 +11,6 @@ def remove_html_tags(raw):
     return re.sub(regex, " ", raw)
 
 
-def lect_get_data(self):
-    client = HTTPClient()
-
-    resp = client.get(self.path)
-
-    if resp is not None:
-        tree = etree.fromstring(resp.encode())
-
-        self.display_title = tree.find("displaytitle").text
-        self.event = tree.find("event").text
-        self.prokeimenon = tree.find("prokeimenon").text
-
-        icon = tree.find("icon")
-        i_file_path = icon.find("url").text.replace("http://onlinechapel.goarch.org/images/", "")
-        i_copyright = icon.find("copyright").text
-
-        self.icon = Icon(i_file_path, i_copyright)
-
-        self.translations = []
-
-        for translation in tree.findall("translation"):
-            lang = translation.get("xml:lang")
-            title = translation.find("title").text
-            short_title = translation.find("shorttitle").text
-            clip = translation.find("clip").text.strip()
-            body = remove_html_tags(translation.find("body").text).strip()
-
-            if self.type == "epistle":
-                prok_mode = translation.find("prokmode").text
-                prok_psalm = translation.find("prokpsalm").text.replace(": ", ":")
-                prok_prokeimenon = translation.find("prokprokeimenon").text
-                prok_verse = translation.find("prokverse").text
-
-                converted_translation = Translation(lang=lang, title=title, short_title=short_title,
-                                                    clip=clip, body=body, prok_mode=prok_mode,
-                                                    prok_psalm=prok_psalm, prok_prokeimenon=prok_prokeimenon,
-                                                    prok_verse=prok_verse)
-            else:
-                converted_translation = Translation(lang=lang, title=title, short_title=short_title,
-                                                    clip=clip, body=body)
-
-            self.translations.append(converted_translation)
-
-
 class Lectionary:
     # this class will provide full readings from Reading objects
     def __init__(self, _type, code, event, date=None):
@@ -63,15 +19,52 @@ class Lectionary:
         self.event = event
         self.date = date
 
-        self.path = f"/lectionary.asp?type={_type}&code={str(code)}&event={str(event)}"
+        self._path = f"/lectionary.asp?type={_type}&code={str(code)}&event={str(event)}"
         self.public_url = f"https://www.goarch.org/chapel/lectionary?type={_type}&code={str(code)}&event={str(event)}"
 
         if date is not None:
-            self.path += f"&date={date}"
+            self._path += f"&date={date}"
             self.public_url += f"&date={date}"
 
-        lect_get_data(self)
+    def get_data(self):
+        client = HTTPClient()
 
+        resp = client.get(self._path)
 
-if __name__ == "__main__":
-    lect = Lectionary("MG", 370, 165)
+        if resp is not None:
+            tree = etree.fromstring(resp.encode())
+
+            self.display_title = tree.find("displaytitle").text
+            self.event = tree.find("event").text
+            self.prokeimenon = tree.find("prokeimenon").text
+
+            icon = tree.find("icon")
+            i_file_path = icon.find("url").text.replace("http://onlinechapel.goarch.org/images/", "")
+            i_copyright = icon.find("copyright").text
+
+            self.icon = Icon(i_file_path, i_copyright)
+
+            self.translations = []
+
+            for translation in tree.findall("translation"):
+                lang = translation.get("xml:lang")
+                title = translation.find("title").text
+                short_title = translation.find("shorttitle").text
+                clip = translation.find("clip").text.strip()
+                body = remove_html_tags(translation.find("body").text).strip()
+
+                if self.type == "epistle":
+                    prok_mode = translation.find("prokmode").text
+                    prok_psalm = translation.find("prokpsalm").text.replace(": ", ":")
+                    prok_prokeimenon = translation.find("prokprokeimenon").text
+                    prok_verse = translation.find("prokverse").text
+
+                    converted_translation = Translation(lang=lang, title=title, short_title=short_title,
+                                                        clip=clip, body=body, prok_mode=prok_mode,
+                                                        prok_psalm=prok_psalm, prok_prokeimenon=prok_prokeimenon,
+                                                        prok_verse=prok_verse)
+                else:
+                    converted_translation = Translation(lang=lang, title=title, short_title=short_title,
+                                                        clip=clip, body=body)
+
+                self.translations.append(converted_translation)
